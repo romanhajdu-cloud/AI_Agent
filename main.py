f714b585214a50eaf7793e3e831183e4b30e24cd
romanhajdu-cloud <romanhajdu@gmail.com>
2026-03-31 22:49:23 +0200

    Add some basic function

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+
+    if response.usage_metadata != None: 
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        raise ValueError 
+
+    print(f"Response: \n{response.text}")
 
 if __name__ == "__main__":
     main()
