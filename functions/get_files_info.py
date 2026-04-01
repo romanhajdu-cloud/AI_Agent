b3751794272c18cf944a6f1ece38924892d1dc12
romanhajdu-cloud <romanhajdu@gmail.com>
2026-04-01 20:06:47 +0200

    Add test function

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+        result = []
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+            file_size = os.path.getsize(item_path)
+            is_dir = os.path.isdir(item_path)
+            result.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+        
+        return "\n".join(result)
+    except Exception as e:
+        return f"Error: {e}"
--- main.py
+++ main.py
@@ -13,16 +13,16 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
 
-    if response.usage_metadata != None: 
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise ValueError 
-
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}") 
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: \n{response.text}")
 
 if __name__ == "__main__":
