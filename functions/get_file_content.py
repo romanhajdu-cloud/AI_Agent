59640b4dd4d42a412394b48b65e0d17edf91a502
romanhajdu-cloud <romanhajdu@gmail.com>
2026-04-05 21:33:53 +0200

    Add more function to call

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
-import config
+from config import MAX_CHARS
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
     try:
@@ -14,11 +15,26 @@ def get_file_content(working_directory, file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
 
         with open(target_file, "r") as f:
-            content = f.read(config.MAX_CHARS)
+            content = f.read(MAX_CHARS)
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {config.MAX_CHARS} characters]'
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
         return content
         
     except Exception as e:
         return f"Error: {e}"
+    
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Retrieves the content (at most {MAX_CHARS} characters) of a specified file within the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read, relative to the working directory",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
     
